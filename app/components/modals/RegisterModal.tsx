b9f10da96ae2dd85bae6f5070614ef4b84843fab
Vitor Nunes <stecks10@gmail.com>
2023-06-29 10:05:36 -0300

    registerModal

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import axios from 'axios'
+import { AiFillGithub } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import { useCallback, useEffect, useState } from 'react'
+import Input from '../inputs/Input'
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm
+} from 'react-hook-form'
+
+import useRegisterModal from '../hooks/useRegisterModal'
+import Modal from './Modal'
+import Heading from '../Heading'
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    }
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios.post('/api/register', data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  }
+
+  const bodyContent = (
+    <div className='flex flex-col gap-4 text-black'>
+      <Heading title="Welcome to Airbnb" subtitle="Register your account" />
+      <Input id='email' label='Email' disabled={isLoading} register={register} errors={errors} />
+      <Input id='Name' label='Name' disabled={isLoading} register={register} errors={errors} />
+      <Input id='Password' label='Password' disabled={isLoading} register={register} errors={errors} />
+    </div>
+  )
+
+  return (
+
+    <Modal disabled={isLoading} isOpen={registerModal.isOpen} title='Register' actionLabel='Continue' onClose={registerModal.onClose} onSubmit={handleSubmit(onSubmit)} body={bodyContent} />
+  )
+}
+
+export default RegisterModal
