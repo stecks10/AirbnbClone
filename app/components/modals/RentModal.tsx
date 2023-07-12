a4a0f2d681ced5886e1f053ff2e0823666fef707
Vitor Nunes <stecks10@gmail.com>
2023-07-12 14:28:03 -0300

    Maps | location

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,7 +8,8 @@ import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
-import Map from '../Map'
+import dynamic from 'next/dynamic';
+
 
 enum STEPS {
   CATEGORY = 0,
@@ -49,6 +50,8 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+
+  const Map = useMemo(() => dynamic(() => import('../Map'), { ssr: false }), [location]);
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -115,7 +118,9 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue('location', value)}
         />
-        <Map />
+        <Map
+          center={location?.latlng}
+        />
       </div>
     )
   }
